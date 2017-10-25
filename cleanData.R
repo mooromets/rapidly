b923c33a407199c71a233cabc2399470fbbed050
mooromets <sergey.sambor@gmail.com>
2017-10-25 19:55:15 +0200

    cleaning punctuation marks is finished

--- cleanData.R
+++ cleanData.R
@@ -1,5 +1,21 @@
 library(tm)
 corp <- SimpleCorpus(DirSource("./data/"))
 
+#remove 2+ puctuation marks in a row 
+corp <- tm_map(corp, content_transformer(gsub), 
+               pattern = "[[:punct:]]{2,}", 
+               replacement = " ")
+# leave only letters, numbers and three more puctuation marks that are
+# the parts of a language (cost-effective, that's, e.g.)
+corp <- tm_map(corp, content_transformer(gsub), 
+               pattern = "[^[:alnum:].'-]", 
+               replacement = " ")
+# remove hanging punctuation marks
+corp <- tm_map(corp, content_transformer(gsub), 
+               pattern = " [-'.] ", 
+               replacement = "")
+# remove dots, that are most likely an end of a sentence 
+corp <- tm_map(corp, content_transformer(gsub), 
+               pattern = "[A-Za-z0-9][.]( +[A-Z]|$)", 
+               replacement = " ")
 corp <- tm_map(corp, stripWhitespace)
-corp <- tm_map(corp, content_transformer(tolower))
--- sampleData.R
+++ sampleData.R
@@ -3,7 +3,7 @@ fileOut <- file("./data/sample.txt", "w")
 
 repeat {
   inData <- readLines(fileIn, n = 1000, skipNul = TRUE)
-  outData <- sample(inData, 50)
+  outData <- sample(inData, 20)
   writeLines(outData, fileOut)
   
   if (length(inData) != 1000) {
