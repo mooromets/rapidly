f41febc7616aa072fccf551d3cd1563ef91056f3
mooromets <sergey.sambor@gmail.com>
2017-10-27 20:51:33 +0200

    VCorpus for bigrams

--- cleanData.R
+++ cleanData.R
@@ -1,5 +1,16 @@
 library(tm)
+library(RWeka)
 
-system.time({
-  corp <- SimpleCorpus(DirSource("./data/chunks/"))
-})
+system.time(
+corpV <- VCorpus(DirSource("./data/chunks/"))
+)
+
+tdm1 <- TermDocumentMatrix(corpV)
+
+nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+
+system.time(
+tdm2 <- TermDocumentMatrix(corpV, list(tokenize = nGramTok, wordLengths=c(1,Inf)))
+)
+
+head(findFreqTerms(tdm2, 5000), 40)
