ba3a728f60710d02dd81b6f512f70bcb2af1f6b5
mooromets <sergey.sambor@gmail.com>
2017-10-28 21:38:41 +0200

    removed debug info from dataClean

--- cleanData.R
+++ cleanData.R
@@ -1,11 +1,7 @@
 library(tm)
 library(RWeka)
 
-system.time(
 corpV <- VCorpus(DirSource("./data/chunks/"))
-)
-
-tdm1 <- TermDocumentMatrix(corpV, list(tolower = FALSE))
 
 nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2, 
                                           delimiters = ' \r\n\t,;:"()?!'))
@@ -14,5 +10,3 @@ tdm2 <- TermDocumentMatrix(corpV, list(tokenize = nGramTok,
                                        wordLengths=c(1,Inf),
                                        tolower = FALSE))
 )
-
-#head(findFreqTerms(tdm2, 10000), 20)
