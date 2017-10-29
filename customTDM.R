8d2ed0c1d6459e439aef232d3bbb684df59c9e04
mooromets <sergey.sambor@gmail.com>
2017-10-29 17:25:01 +0100

    replaced 'my' with 'custom'

--- customTDM.R
+++ customTDM.R
@@ -10,12 +10,12 @@ ctrlList <- list(wordLengths = c(1,Inf),
 customDelimiters <- ' \r\n\t,;:"()?!' # without ['-.]
 
 # single-word TDM
-myTDM1 <- function (corpus) {
+customTDM1 <- function (corpus) {
   TermDocumentMatrix(corpus, ctrlList)
 }
 
 # nGram TDM
-myTDMn <- function (corpus, nGramMin, nGramMax = nGramMin) {
+customTDMn <- function (corpus, nGramMin, nGramMax = nGramMin) {
   nGramTok <- function(x) NGramTokenizer(x, Weka_control(min = nGramMin, 
                                                          max = nGramMax, 
                                               delimiters = customDelimiters))
--- extractTerms.R
+++ extractTerms.R
@@ -10,11 +10,11 @@ filesList <- paste0(textDir, dir(textDir))
 corpV <- VCorpus(DirSource(filesList[1]))
 
 # single-word term-document-matrix
-leftTDM1 <- myTDM1(corpV)
+leftTDM1 <- customTDM1(corpV)
 
 # bigram tdm
-leftTDM2 <- myTDMn(corpV, 2)
+leftTDM2 <- customTDMn(corpV, 2)
 
-leftTDM3 <- myTDMn(corpV, 3)
+leftTDM3 <- customTDMn(corpV, 3)
 
-leftTDM4 <- myTDMn(corpV, 4)
+leftTDM4 <- customTDMn(corpV, 4)
