a0cbfcfdab62cd72d665ce0aa5675f20498a6bc6
mooromets <sergey.sambor@gmail.com>
2017-10-30 02:57:10 +0100

    report is finished

--- sampleData.R
+++ sampleData.R
@@ -1,15 +1,19 @@
-fileIn <- file("./data/final/en_US/en_US.blogs.txt", "r")
+dirPath <- "data/final/en_US/"
+filesList <- paste0(dirPath, dir(dirPath))
 fileOut <- file("./data/small_sample.txt", "w")
 
-repeat {
-  inData <- readLines(fileIn, n = 1000, skipNul = TRUE)
-  outData <- sample(inData, 20)
-  writeLines(outData, fileOut)
-  
-  if (length(inData) != 1000) {
-    break
+for (f in filesList) {
+  fileIn <- file(f, "r")
+  repeat {
+    inData <- readLines(fileIn, n = 1000, skipNul = TRUE)
+    outData <- sample(inData, 20)
+    writeLines(outData, fileOut)
+    
+    if (length(inData) != 1000) {
+      break
+    }
   }
-}
+  close(fileIn)
+}  
 
-close(fileIn)
 close(fileOut)
