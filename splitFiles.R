169e5489901e8a65ab9054fa5edab828d3806e28
mooromets <sergey.sambor@gmail.com>
2017-10-30 18:21:42 +0100

    splitting the data into train/test/validate subsets

--- splitFiles.R
+++ splitFiles.R
@@ -1,4 +1,5 @@
 # split big 'final' files into small fast-processible chunks
+# also split data into three subsets: training, testing, validating
 
 source("textCleaner.R")
 source("censure.R")
@@ -13,36 +14,62 @@ if (!dir.exists(dirOut))
 remFiles <- dir(dirOut)
 if (length(remFiles) > 0)
   remFiles <- paste0(dirOut, remFiles)
-file.remove(remFiles)
+file.remove(remFiles, recursive = TRUE)
+
+trainDataDir <- paste0(dirOut, "train/")
+dir.create(trainDataDir)
+testDataDir <- paste0(dirOut, "test/")
+dir.create(testDataDir)
+validateDataDir <- paste0(dirOut, "validate/")
+dir.create(validateDataDir)
+
+# prepare test files
+testFile <- file(paste0(testDataDir, "testChunk.txt"), "wt")
+validateFile <- file(paste0(validateDataDir, "validateChunk.txt"), "wt")
 
 # list input files
 filesList <- paste0(dirPath, dir(dirPath))
 
 chunkNum <- 0
+cleaningElapsed <- c()
 for (f in filesList) {
   fileIn <- file(f, "r")
   repeat {
     # create a new file and write lines
     chunkNum <- chunkNum + 1
-    fileOut <- file(paste0(dirOut, "chunk", as.character(chunkNum), ".txt"), "w")
+    #separate dir for every file in train subset
+    newDir <- paste0(trainDataDir, as.character(chunkNum), "/")
+    dir.create(newDir) 
+    fileOut <- file(paste0(newDir, "chunk", as.character(chunkNum), ".txt"), "wt")
     inData <- readLines(fileIn, n = LPF, skipNul = TRUE)
-    linesRead <- length(inData)
-    inData <- textCleaner(inData, censured = censuredWords())
-    writeLines(inData, fileOut)
+    # indexes for 3 subsets
+    trainIndex <- 1:length(inData)
+    testIndex <- sample(trainIndex, max(50, length(trainIndex) * .01))
+    trainIndex <- setdiff(trainIndex, testIndex)
+    validateIndex <- sample(testIndex, max(10, length(testIndex) * .1))
+    testIndex <- setdiff(testIndex, validateIndex)
+    # making subsets
+    st <- system.time( {
+      trainData <- textCleaner(inData[trainIndex], 
+                               censured = censuredWords())
+      testData <- textCleaner(inData[testIndex], 
+                              censured = censuredWords())
+      validateData <- textCleaner(inData[validateIndex], 
+                                  censured = censuredWords())
+    })
+    cleaningElapsed <- c(cleaningElapsed, st[3])
+    writeLines(testData, testFile)
+    writeLines(validateData, validateFile)
+    writeLines(trainData, fileOut)
     close(fileOut)  
-    
-    if (linesRead < LPF) {# last chunk from input data
+    if (length(inData) < LPF) {# last chunk from input data
       break
     }
   }
   close(fileIn)
 }
+close(testFile)
+close(validateFile)
 
-# place every chunk to a separate folder 
-filesList <- dir(dirOut)
-for (i in 1:length(filesList)) {
-  newDirName <- as.character(i)
-  dir.create(paste0(dirOut, newDirName))
-  file.rename(from = paste0(dirOut, filesList[i]),
-              to = paste0(dirOut, newDirName, "/" , filesList[i]))
-}
+print(paste("Cleaning elapsed time:", as.character(sum(cleaningElapsed))))
+print(cleaningElapsed)
