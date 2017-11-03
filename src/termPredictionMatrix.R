1352566bf5d55072cc1f8c3e08cc00a487bf01d0
mooromets <sergey.sambor@gmail.com>
2017-11-03 16:28:45 +0100

    sparse matrices and Markov Chains implemented for terms

--- src/termPredictionMatrix.R
+++ src/termPredictionMatrix.R
@@ -31,6 +31,23 @@ mostFreqUnique <- function(TPM) {
     }))
 }
 
+mostFreqUnique2 <- function(TPM, portionSize = 100) {
+  idxL <- 0
+  outData <- data.frame()
+  repeat {
+    idxH <- min(idxL + portionSize, nrow(TPM))
+    while (idxH < nrow(TPM) && TPM[idxH, "pred"] == TPM[idxH + 1, "pred"]) {
+      idxH <- idxH + 1
+    }
+    outData <- rbind(outData, mostFreqUnique(TPM[idxL:idxH, ]))
+    if (idxH == nrow(TPM)) {
+      break
+    }
+    idxL <- idxH
+  }
+  outData
+}
+
 mostFreqNotUnique <- function(TPM) {
   do.call(rbind, lapply(unique(TPM$pred), function(term) {
     TPM %>% 
@@ -51,5 +68,51 @@ tpm <- function(input) {
     group_by(pred, outcome) %>%
     summarise(freq = sum(freq.x))
   # calculate probabilities
-  mostFreqUnique(input)
+   mostFreqUnique2(input)
+  #input
+}
+
+# Term-prediction matrix
+tpm2 <- function(input) {
+  input[, 3:4] <- splitTerm(input) # split terms to predictor and outcome
+  #input$V1 <-stemNgram(input$V1) # stemming predictor only
+  # stemming pros not proved yet
+  input$V1 <-tolower(input$V1) # predictor to lower case
+  # remove duplicates
+  input <- input %>% 
+    rename(pred = V1, outcome = V2) %>%
+    group_by(pred, outcome) %>%
+    summarise(freq = sum(freq.x))
+  # calculate probabilities
+  require(Matrix)
+  is <- unique(input$pred)
+  js <- unique(input$outcome)
+  sparseMatrix( as.factor(input$pred),
+                as.factor(input$outcome),
+                x = income$freq)
+  #input
+}
+
+# Term-prediction matrix
+sparseTPM <- function(input) {
+  print(system.time({
+  input[, 3:4] <- splitTerm(input) # split terms to predictor and outcome
+  #input$V1 <-stemNgram(input$V1) # stemming predictor only
+  # stemming pros not proved yet
+  input$V1 <-tolower(input$V1) # predictor to lower case
+  # remove duplicates
+  input <- input %>% 
+    rename(pred = V1, outcome = V2) %>%
+    group_by(pred, outcome) %>%
+    summarise(freq = sum(freq.x))
+  require(Matrix)
+  is <- as.factor(input$pred)
+  js <- as.factor(input$outcome)
+  spa <- sparseMatrix( as.integer(is),
+                       as.integer(js),
+                       x = input$freq)
+  colnames(spa) = levels(js)
+  rownames(spa) = levels(is)
+  }))
+  spa
 }
