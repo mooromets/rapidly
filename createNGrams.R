526fd42f95c1373fe2ba42a4db8a11c09ee33927
mooromets <sergey.sambor@gmail.com>
2017-11-09 22:24:28 +0100

    generalized nGram terms creation for all sizes

--- createNGrams.R
+++ createNGrams.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 source("src/customTDM.R")
 
-print(Sys.time())
+#print(Sys.time())
 
 chunksPath <- "data/chunks/train/"
 
@@ -16,47 +16,59 @@ term2ID <- list2env(tmpList, hash = TRUE)
 ID2Term <- as.vector(df$term)
 rm(df)
 rm(tmpList)
-gc()
+#gc()
 
-dirList <- paste0(chunksPath, dir(chunksPath))
+# doJob function makes all work regarding one nGram (bigram/trigram/etc.) 
+#TODO refactor this monsterous function
+doJob <- function(N) {
+  
+  dirList <- paste0(chunksPath, dir(chunksPath))
 
-getTermsIds <- function(dirname) {
-  corp <- VCorpus(DirSource(dirname))
-  mtx <- as.matrix(customTDMn(corp, 2))
-  rm(corp)
-  ids <- do.call(rbind, lapply(seq_along(mtx), function(idx) {
-    matrix(data = c(as.vector(
-                      sapply(unlist(strsplit(rownames(mtx)[idx], " ")), 
-                             function(x) as.numeric(term2ID[[x]])),
-                      mode = "numeric"
-                    ), 
-                    mtx[idx, 1]),    
-          nrow = 1)
-  }))
-  rm(mtx)
-  gc()
-  # remove phrases with words out of dictionary
-  as.data.frame(ids[complete.cases(ids), ])
+  # getTermsIds function returns a data.frame containing all terms from the 
+  # corpus from input directory
+  getTermsIds <- function(dirname) {
+    corp <- VCorpus(DirSource(dirname))
+    mtx <- as.matrix(customTDMn(corp, N))
+    rm(corp)
+    # replace all words in matrix with their IDs from dictionary
+    ids <- do.call(rbind, lapply(seq_along(mtx), function(idx) {
+      matrix(data = c(as.vector(
+                        sapply(unlist(strsplit(rownames(mtx)[idx], " ")), 
+                               function(x) as.numeric(term2ID[[x]])),
+                        mode = "numeric"
+                      ), 
+                      mtx[idx, 1]),    
+            nrow = 1)
+    }))
+    rm(mtx)
+    #gc()
+    # words absent in dictionary == NA, remove those lines
+    as.data.frame(ids[complete.cases(ids), ]) 
+  }
+  #print(Sys.time())
+  idsLeft <- getTermsIds(dirList[1])
+  termColumns <- c(1:N) # collumns in data frame, that are words and not frequency
+  freqColumn <- N+1 # column(s) containing frequency
+  #gc()
+  for (idir in 2:5) { #length(dirList)) {
+    idsRight <- getTermsIds(dirList[idir])
+    idsLeft <- full_join(idsLeft, 
+                         idsRight, 
+                         by = paste0("V", as.character(termColumns)))
+    idsLeft[, freqColumn] <- apply(idsLeft[, -termColumns], 1, sum, na.rm = TRUE)
+    idsLeft <- idsLeft[, c(termColumns, freqColumn)]
+    print(object.size(idsLeft))
+    print(Sys.time())
+    #gc()  
+  }
+  
+  write.csv(idsLeft, "data/fullBigramms.csv", row.names = FALSE)
+  # filter wrong spelled words
+  write.csv(idsLeft[idsLeft[, 3] > 2, ], 
+            "data/cleanBigramms.csv",
+            row.names = FALSE)
+  
+  #here sparse matrix code
 }
 
-print(Sys.time())
-
-idsLeft <- getTermsIds(dirList[1])
-gc()
-
-for (idir in 2:7) { #length(dirList)) {
-  idsRight <- getTermsIds(dirList[idir])
-
-  idsLeft <- full_join(idsLeft, idsRight, by = c("V1","V2"))
-  idsLeft[, 3] <- apply(idsLeft[, -1:-2], 1, sum, na.rm = TRUE)
-  idsLeft <- idsLeft[, 1:3]
-  print(object.size(idsLeft))
-  print(Sys.time())
-  gc()  
-}
-
-write.csv(idsLeft, "data/fullBigramms.csv", row.names = FALSE)
-# filter wrong spelled words
-write.csv(idsLeft[idsLeft[, 3] > 2, ], 
-          "data/cleanBigramms.csv",
-          row.names = FALSE)
+doJob(2)
