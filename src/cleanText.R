e86b67963708f9f84b52997cf1134e7ee6d84bfc
mooromets <sergey.sambor@gmail.com>
2017-12-17 19:26:56 +0100

    cleanText.R comments updated

--- src/cleanText.R
+++ src/cleanText.R
@@ -4,36 +4,77 @@ library(tm)
 
 source("src/common.R")
 
+
+#' removeStandaloneLetters
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 removeStandaloneLetters <- function(x) {
   removeWords(x, c("b", "c", "e", "f", "g", "h", "j", "k", "l",
                    "p", "q", "r", "u", "v", "w", "x", "y", "z"))  
 }
 
+
+#' recoverApostrophe
+#' 
+#' recover a missed apostrohe in words
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 recoverApostrophe <- function(x) {
   gsub(pattern = "([[:alpha:]]) (d|ll|ve|s|re|m|t)([[:space:]]|$)", 
        replacement = "\\1'\\2\\3", 
        x)
 }
 
+
+#' removePuctLeaveApost
+#' 
+#' remove all punctation marks except apostrophe
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 removePuctLeaveApost <- function(x) {
   gsub(pattern = "[^[:alnum:]'[:space:]]", 
        replacement = " ", 
        x)
 }
 
+
+#' removeConseqApost
+#' 
+#' remove consequetive apostrophes
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 removeConseqApost <- function(x) {
   gsub(pattern = "'{2,}", 
        replacement = "", 
        x)
 }
 
+
+#' sentenceSplit
+#' 
+#' split every line into lines containing one sentence each
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 sentenceSplit <- function(x) {
   unlist(strsplit(x, 
                   split = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=\\.|\\?)\\s", 
                   perl = TRUE))
 }
 
-# get (read) censured words as pattern to gsub
+
+#' censuredWords
+#' 
+#' @return a character vector of censured words
 censuredWords <- function(filename = "data/censure.txt") {
   censured <- ""
   cens <- file(filename, "r")
@@ -52,6 +93,12 @@ censuredWords <- function(filename = "data/censure.txt") {
   censured
 }
 
+
+#' cleanText
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 cleanText <- function(x) {
   x <- tolower(x)
   x <- removeNumbers(x)
@@ -63,6 +110,14 @@ cleanText <- function(x) {
   x <- stripWhitespace(x)
 }
 
+
+#' cleanInput
+#' 
+#' performs a text cleaning on a text inputted in UI
+#' 
+#' @param x a character vector
+#' 
+#' @return a cleaned character vector
 cleanInput <- function(x) {
   if (length(x) == 0)
     return (NULL)
