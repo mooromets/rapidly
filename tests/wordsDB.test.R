0d292bd78280d9972bd68ba96a6be0a6fced6730
mooromets <sergey.sambor@gmail.com>
2017-12-17 17:27:08 +0100

    WordsDB basic untit tests

---
+++ tests/wordsDB.test.R
@@ -0,0 +1,26 @@
+require(testthat)
+setwd("..")
+source("src/wordsDB.R")
+
+context("WordsDB tests")
+
+test_that("creation of WordsDB, opening a connection", {
+  wdb <- WordsDB()
+  expect_true(dbIsValid(wdb$dataConn))
+})
+
+test_that("words and IDs getting", {
+  wo <- c("home", "crow", "blue")
+  ids <- wdb$getWordID(wo)
+  expect_equal(length(ids), length(wo))
+  getwo <- wdb$getWord(ids)
+  expect_equal(getwo, wo)
+  #NA
+  wo <- c("home", "NAword", "blue")
+  ids <- wdb$getWordID(wo)
+  expect_equal(length(ids), length(wo))
+  expect_equivalent(ids[2], as.numeric(NA))
+  getwo <- wdb$getWord(ids)
+  expect_equal(length(getwo), length(wo))
+  expect_equivalent(getwo[2], ".NA.")
+})
