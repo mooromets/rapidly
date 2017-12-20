1aedc8b82ceee4ff3749bd7f94c37250cfff1e78
mooromets <sergey.sambor@gmail.com>
2017-12-20 18:38:28 +0100

    moved modelData.R to scripts

--- script/pickFrequencyThreshold.R
+++ script/pickFrequencyThreshold.R
@@ -5,7 +5,7 @@ for (thres in 1:15) {
   cat(sprintf("--- thres = %d ---", thres))
   print(Sys.time())
   commandArgs <- function(...) thres
-  source("src/modelData.R")
+  source("script/modelData.R")
   print(sum(sapply(sparseTpmList, function(m) object.size(m) / 1024 ^ 2)))
   print(Sys.time())
   source("src/modeling.R")
