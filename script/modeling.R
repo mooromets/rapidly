1e3237694be7583c2b11782bdfd6f1d4bdabc776
mooromets <sergey.sambor@gmail.com>
2017-12-27 23:49:26 +0100

    modeling.R moved to scripts

--- script/modeling.R
+++ script/modeling.R
@@ -1,6 +1,4 @@
-#source("src/modelData.R")
 source("src/models.R")
-#source("src/termMatrixOps.R")
 source("src/search.R")
 source("src/benchmark.R")
 source("src/common.R")
@@ -9,6 +7,4 @@ getNextWord <- function(term, ...) {
   lookDB(term, bestWithMask, ...)
 }
 
-#benchmark(benchmarkText(), etime = 300, getNextWord)
-
-print(getNextWord("let it go dog"))
+benchmark(benchmarkText(), etime = 300, getNextWord)
