5b9324a9f3bce710da21e05fe306d5fb74d1b6dd
mooromets <sergey.sambor@gmail.com>
2017-12-30 15:19:00 +0100

    outdated models removed

--- src/models.R
+++ src/models.R
@@ -1,63 +1,7 @@
-#different models
+# different models for picking the next word predictions
+
 library(dplyr)
 source("src/monitor.R")
-
-maxFUN <- function(row, dictVec) {
-  max <-order(row, decreasing = TRUE)[1:5]
-  return(c(unlist(sapply(max, function(x) dictVec[x] ))))
-}
-
-gatherFUN <- function (x) x
-
-#'
-#' get 5 values from row with the highest probability
-#' @param row a input vector with frequencies of words
-#' @param dictVec dictionary with words
-#' @return a numeric vector of 5 probabilities with their words as names 
-#'
-fiveMostProb <- function(row, dictVec) {
-  if (sum(row) <= 0) return (NULL)
-  row <- row / sum(row)
-  max5 <- order(row, decreasing = TRUE)[1:5]
-  result <- row[max5]
-  names(result) <- c(unlist(sapply(max5, function(x) dictVec[x])))
-  result
-}
-
-choose3top <- function(probList) {
-  #concatenate all results
-  all <- c()
-  for (i in seq_along(probList)) {
-    all <- c(all, probList[[i]])
-  }
-  res <- c()
-  for (i in seq_len(3)) {
-    maxIdx <- which.max(all)
-    # add this max to result
-    res <- c(res, names(all)[maxIdx])
-    # remove all apearance of this word from all 
-    all <- all[names(all) != names(all)[maxIdx]]
-  }
-  return(res)
-}
-
-stupidBackoff <- function(probList, lambda = 0.5) {
-  if (length(probList) == 0) return(c("the", "on", "a"))
-  #concatenate all results
-  all <- c()
-  for (i in seq(length(probList), 1)) {
-    all <- c(all, probList[[i]] * lambda^(length(probList) - i))
-  }
-  res <- c()
-  for (i in seq_len(3)) {
-    maxIdx <- which.max(all)
-    # add this max to result
-    res <- c(res, names(all)[maxIdx])
-    # remove all apearance of this word from all 
-    all <- all[names(all) != names(all)[maxIdx]]
-  }
-  return(res)
-}
 
 
 #' bestWithMask
